83aee80a505626c2b27181af27c622589b6a99ac
Kris De Volder <kdevolder@gopivotal.com>
2013-12-20 10:20:46 -0800

    Cleanup some unneeded dependencies.

--- src/main/java/org/springsource/ide/eclipse/boot/maven/analyzer/server/RestServer.java
+++ src/main/java/org/springsource/ide/eclipse/boot/maven/analyzer/server/RestServer.java
@@ -6,11 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
 
 /**
  * Spring Boot Application to serve serve jar-type content assist type graph
